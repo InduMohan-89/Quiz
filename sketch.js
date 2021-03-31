f221ca98399bf50ecf5552483cca591d9a8e3e89
InduMohan-89 <73943698+InduMohan-89@users.noreply.github.com>
2021-03-31 14:57:51 +0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+var canvas,database;
+var gameState = 0;
+var contestantCount=0;
+var database, quiz, question,contestant;
+var allContestants;
+
+function setup(){
+  canvas = createCanvas(850,400);
+database = firebase.database();
+quiz = new Quiz();
+quiz.getState();
+quiz.start();
+
+}
+
+
+function draw(){
+  background("pink");
+  if(contestantCount === 4){
+    quiz.update(1);
+  }
+  if(gameState === 1){
+    clear();
+    quiz.play();
+  }
+  
+}
